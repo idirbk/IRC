86dd773e090111c2c20f6651d60970fcbb30c331
idirbk <97450551+idirbk@users.noreply.github.com>
2024-01-14 15:59:49 +0100

    create a message class

---
+++ message.py
@@ -0,0 +1,10 @@
+
+class Message:
+    def __init__(self, sender, receiver, payload, receiverIsChannel=False):
+        self.sender = sender
+        self.receiver = receiver
+        self.payload = payload
+        self.receiverIsChannel = receiverIsChannel
+    
+    def __str__(self):
+        return ';'.join([self.sender, self.receiver, self.payload, self.receiverIsChannel])
