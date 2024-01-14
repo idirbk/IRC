edc55ed349e2d0974ed6f6f46e5a350f93449796
idirbk <97450551+idirbk@users.noreply.github.com>
2024-01-14 17:57:15 +0100

    adapt channel class with users class

--- channel.py
+++ channel.py
@@ -3,17 +3,31 @@ from message import Message
 class Channel:
     def __init__(self, channel_name: str):
         self.channel_name = channel_name
-        self.members = []
+        self.users = []
         self.messages = []
 
-    def getChannelMembers(self) -> list(str):
-        return self.members
+    def getChannelUsers(self) -> list(str):
+        return self.users
 
     def getChannelMessages(self) -> list(Message):
         return self.messages
 
     def publishMessage(self, message: Message) -> None:
+        message.id = len(self.messages)
         self.messages.append(message)
 
-    def addMember(self, member) -> None:
-        self.members.append(member)
+    def addUember(self, member) -> None:
+        self.users.append(member)
+    
+    def disconnectUser(self, username):
+        for user in self.users:
+            if user.username == username:
+                user.disconnect()
+    
+    def connectUser(self, username):
+        for user in self.users:
+            if user.username == username:
+                user.connect()
+    
+    def getConnectedUsers(self):
+        return filter(lambda user: user.isConnected(), self.users)
