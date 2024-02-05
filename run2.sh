f22deef54ce649838f988310cfb09a2274aff0d9
idirbk <97450551+idirbk@users.noreply.github.com>
2024-02-05 11:49:35 +0100

    messages between users in diferent servers

--- run2.sh
+++ run2.sh
@@ -2,13 +2,13 @@
 
 # Start the Python server
 echo "Starting the server..."
-python3 server.py 8284 &
+python3 server.py 8285 8284&
 SERVER_PID=$!
 sleep 2  # Gives the server time to start up
 
 # Start two instances of the Python client
 echo "Starting the first client..."
-python3 userInterface.py 8284 idir&
+python3 userInterface.py 8285 farid&
 #echo "Starting the second client..."
 #python3 userInterface.py 8380 farid&
 
--- server.py
+++ server.py
@@ -32,17 +32,31 @@ class IRCServer:
         else:
             return None
 
-    def start_server(self):
+    def start_server(self, servers_ports):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.host, self.port))
         self.sock.listen(5)
         logging.info(f"IRC Server started on {self.host}:{self.port}")
-
+        self.connect_to_servers(servers_ports)
         while True:
             client, address = self.sock.accept()
-            #client.settimeout(0.5)
+            client.settimeout(0.5)
             logging.info(f"Connection from {address}")
             threading.Thread(target=self.handle_connection, args=(client,)).start()
+
+    def send_to_all_servers(self, command, server_sender = None):
+        for server in self.servers:
+            
+            if server_sender and server_sender != server['port']:
+                server['lock'].acquire()
+                logging.debug(f"sending to server {server['port']}")
+                server['server'].send(command)
+                msg = server['server'].recv(1024).decode('utf-8')
+                logging.info(f"msg '{msg}'")
+                server['lock'].release()
+                if msg.startswith('True'):
+                    return (True, msg[5:])
+        return (False, '')
 
     def handle_connection(self, connection):
         message = connection.recv(1024).decode().strip()
@@ -56,15 +70,55 @@ class IRCServer:
             if command.lower() == 'server':
                 if len(args) > 0:
                     port = int(args[0])
-                    self.handle_server(connection, nickname=port)
+                    self.handle_server(connection, port=port)
             connection.close()
 
-    def handle_server(self, server, port):
-        self.servers.append({"port": port, "server": server})
+    def connect_to_servers(self, servers_ports):
+        for server_port in servers_ports:
+            port = int(server_port)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('localhost', port))
+            sock.send((f'/server {self.port}').encode('utf-8'))
+            sock.settimeout(0.5)
+            threading.Thread(target=self.handle_server, args=(sock, port,)).start()
 
+    def handle_server(self, server, port):
+        lock = threading.Lock()
+        self.servers.append({"port": port, "server": server, 'lock': lock})
         while True:
-            message = server.recv(1024).decode().strip()
-            logging.debug(f"Server message: {message}")
+            try:
+                #lock.acquire()
+                message = server.recv(1024).decode().strip()
+                logging.debug(f"Server message: {message}")
+                if not message:
+                    continue
+                if message.startswith('/'):
+                    command, *args = message[1:].split()
+                    match command.lower():
+                        case 'msg':
+                            if len(args) < 2:
+                                logging.error('invalid args for msg command')
+                            else:
+                                destination = args[1]
+                                sender = args[0]
+                                message = Message(sender=sender, payload=' '.join(args[2:]))
+                                if channel_destination := self.getChannel(destination):
+                                    channel_destination.send(message)
+                                    logging.info(f"Message sended to the channel :{destination}")
+                                    server.send('True|'.encode('utf-8'))
+                                elif user_dest := self.getUser(destination):
+                                    message.receiver = user_dest.username
+                                    user_dest.send(message)
+                                    server.send('True|'.encode('utf-8'))
+                                else:
+                                    (status, msg)= self.send_to_all_servers(message.payload.encode('utf-8'))
+                                    server.send(('True|' if status else 'False|').encode('utf-8'))
+                                
+                        case _:
+                            logging.error(f"Invalid command : {command}")
+            except TimeoutError:
+                #lock.release()
+                continue
 
     def handle_client(self, client, nickname):
 
@@ -80,7 +134,7 @@ class IRCServer:
                 message = client.recv(1024).decode().strip()
                 if not message:
                     continue
-                logging.debug(f"Received message {message}")
+                logging.debug(f"Received message Client {message}")
                 if message.startswith('/'):
                     command, *args = message[1:].split()
                     match command.lower():
@@ -114,17 +168,19 @@ class IRCServer:
                                 logging.error('invalid args for msg command')
                             else:
                                 destination = args[0]
-                                message = Message(sender=client_user.username, payload=' '.join(args[1:]))
+                                msg = Message(sender=client_user.username, payload=' '.join(args[1:]))
                                 if channel_destination := self.getChannel(destination):
-                                    message.sender = client_user.username
-                                    channel_destination.send(message)
-                                    logging.info(f"Message sended to the channel :{destination}")
+                                    msg.sender = client_user.username
+                                    channel_destination.send(msg)
+                                    logging.info(f"msg sended to the channel :{destination}")
                                 elif user_dest := self.getUser(destination):
-                                    message.receiver = user_dest.username
-                                    user_dest.send(message)
+                                    msg.receiver = user_dest.username
+                                    user_dest.send(msg)
                                 else:
-                                    logging.error('Unknown destination')
-                                    client = self.send('Unknown destination'.encode('utf-8'))
+                                    
+                                    (status, msg)= self.send_to_all_servers(('/msg '+client_user.username + ' '+' '.join(args)).encode('utf-8'), self.port)
+                                    client.send(('True|' if status else 'False|').encode('utf-8'))
+                                   
                                 
                         case 'names':
                             canal = None
@@ -157,5 +213,10 @@ if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
     port = int(sys.argv[1])
+    logging.debug(f'port: {port}')
+    servers_ports = []
+    if len(sys.argv) > 2:
+        servers_ports = sys.argv[2:]
+        
     server = IRCServer('localhost', port)
-    server.start_server()
+    server.start_server(servers_ports)
